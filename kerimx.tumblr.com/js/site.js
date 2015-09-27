8fdb596151e1f0dc66ead7557769e98f51bb8ec8
Kerim Nadir Karsavran <kerimx@outlook.com>
2015-09-27 17:02:36 +0300

    1

--- kerimx.tumblr.com/js/site.js
+++ kerimx.tumblr.com/js/site.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
       itemSelector: '.posts > article',
       percentPosition: true,
       columnWidth: '.grid-sizer',
-	  transitionDuration: '0.2s'
+	  transitionDuration: 0
     });
   });
 });
