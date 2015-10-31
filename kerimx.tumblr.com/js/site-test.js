16b0c5b9b4bd8d82ddf431d53fdf827cd2198402
Kerim Nadir Karsavran <kerimx@outlook.com>
2015-10-31 20:47:42 +0200

    1

---
+++ kerimx.tumblr.com/js/site-test.js
@@ -0,0 +1,52 @@
+//sayfalar
+var homePage = window.location.pathname == '/';
+var postPage = window.location.pathname.indexOf('post');
+var askPage = window.location.pathname.indexOf('ask');
+if (homePage == 1) {
+    $('body').addClass('homePage');
+} else if (postPage > -1) {
+	$('body').addClass('postPage');
+} else if (askPage > -1) {
+    $('body').addClass('askPage');
+} else {
+	$('body').addClass('normalPage');
+	}
+	
+//masonry
+$(document).ready(function () {
+  // init Masonry after all images have loaded
+  var $grid = $('.posts').imagesLoaded(function () {
+    $grid.masonry({
+      itemSelector: '.posts > article',
+      percentPosition: true,
+      columnWidth: '.grid-sizer',
+	  transitionDuration: '0.2s'
+    });
+  });
+});
+	
+/*bossa gizle
+if ($('.asd').html().length < 15) $('.asd').addClass('hidden');*/
+	
+//lightbox
+$('.photo figure > a, .photoset figure > a, article .lb').nivoLightbox({ keyboardNav: true });
+
+//pageTop
+$('li.pageInfo > i').click(function(){
+	$('html, body').animate({scrollTop:0}, 600);
+});
+
+/*
+//mobil menu
+$('header nav > i').click(function(e) {
+	e.stopPropagation();
+    if(!$('header nav').hasClass('open')){
+			$('header nav').addClass('open');
+		}else{
+			$('header nav').removeClass('open');
+			};
+});
+	$(document).click(function(){
+			$('header nav').removeClass('open');
+	});
+*/
